3c383d89eb4e22c8b644c6693a832d07b58c27f0
MKortmann <companymvms@gmail.com>
2019-11-10 18:41:57 +0100

    Updates

---
+++ precache-manifest.6375933b4d2cb96be8aeb2b728c4148b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e25163a920f7fb56a1246c27174822d",
+    "url": "/MKortmann/MyReads/static/media/search.3e25163a.svg"
+  },
+  {
+    "revision": "03a5d818578aad8c5933abf1b092bd9e",
+    "url": "/MKortmann/MyReads/static/media/mk.03a5d818.svg"
+  },
+  {
+    "revision": "09afd391e0364725ec86a2cb2ee57a03",
+    "url": "/MKortmann/MyReads/static/media/arrow-drop-down.09afd391.svg"
+  },
+  {
+    "revision": "dd9d4e193770832065839b185bd8b602",
+    "url": "/MKortmann/MyReads/static/media/arrow-back.dd9d4e19.svg"
+  },
+  {
+    "revision": "c3b06550003283aec39359ac5502b008",
+    "url": "/MKortmann/MyReads/static/media/add.c3b06550.svg"
+  },
+  {
+    "revision": "cdf1e1714026a7397d2f",
+    "url": "/MKortmann/MyReads/static/js/runtime~main.cdf1e171.js"
+  },
+  {
+    "revision": "ba6299e0601db3ed8b5b",
+    "url": "/MKortmann/MyReads/static/js/main.ba6299e0.chunk.js"
+  },
+  {
+    "revision": "2f3e8e779c049180a193",
+    "url": "/MKortmann/MyReads/static/js/1.2f3e8e77.chunk.js"
+  },
+  {
+    "revision": "ba6299e0601db3ed8b5b",
+    "url": "/MKortmann/MyReads/static/css/main.161b70f7.chunk.css"
+  },
+  {
+    "revision": "35784fe5db27bd50cd0fc975dd19db38",
+    "url": "/MKortmann/MyReads/index.html"
+  }
+];
